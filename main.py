857c8451e6f0657731aa88cf24ca4658830ac12c
Peyton Murray <peynmurray@gmail.com>
2016-09-03 22:28:20 -0700

    Multithreaded working now

--- main.py
+++ main.py
@@ -14,32 +14,39 @@ from PyQt5.QtCore import QThread
 
 class QueryVoltmeter(QThread):
 
-	sendData = QtCore.pyqtSignal(float)
+	dataReady = QtCore.pyqtSignal(float)
 
 	def __init__(self, gpib_device, rate):
 		QThread.__init__(self)
 		self.gpib_device = gpib_device
 		self.rm = visa.ResourceManager()
 		self.instrument = self.rm.open_resource(gpib_device)
+		self.rate = rate
+		self.stopFlag = False
 
 	def __del__(self):
 		self.wait()
 
 	def run(self):
-		self.query()
+		while not self.stopFlag:
+			self.dataReady.emit(float(self.instrument.read()[5:]))
 		return
 
-	def query(self):
-		self.sendData.emit(self.instrument.read()[4:])
+	def stop(self):
+		self.stopFlag = True
 		return
 
 class PyVoltageReader(QtWidgets.QMainWindow, PyVoltageReader.Ui_MainWindow, object):
+
+	stopReading = QtCore.pyqtSignal()
 
 	def __init__(self):
 		QtWidgets.QMainWindow.__init__(self)
 		self.setupUi(self)
 		self.connectButtons()
 		self.plot = mplWidget.mplWidget(self, layout=self.mplLayout)
+		self.ax = self.plot.getAxes()
+		self.line, = plot([], [], '-k')
 		self.decoratePlot()
 		return
 
@@ -48,26 +55,46 @@ class PyVoltageReader(QtWidgets.QMainWindow, PyVoltageReader.Ui_MainWindow, obje
 		self.stopButton.clicked.connect(self.stopButtonClicked)
 		self.clearButton.clicked.connect(self.clearButtonClicked)
 		self.refreshButton.clicked.connect(self.refreshButtonClicked)
+
+		self.refreshButtonClicked()
 		return
 
 	def startButtonClicked(self):
-
-		self.queryThread = QueryVoltmeter(gpib_device, rate)
-
+		self.t0 = time.time()
+		self.queryThread = QueryVoltmeter(self.gpibBox.currentText(), rate)
+		self.queryThread.dataReady.connect(self.updatePlot)
+		self.queryThread.start()
 		return
 
 	def stopButtonClicked(self):
+		self.queryThread.stop()
 		return
 
 	def clearButtonClicked(self):
+		self.plot.clearFig()
 		return
 
 	def refreshButtonClicked(self):
+		self.gpibBox.clear()
+		instruments = visa.ResourceManager().list_resources()
+		self.gpibBox.addItems(instruments)
 		return
 
 	def decoratePlot(self):
 		self.plot.getAxes().set_xlabel("Time")
 		self.plot.getAxes().set_ylabel("Voltage")
+		return
+
+	def updatePlot(self, data):
+		self.line.set_data(append(self.line.get_xdata(), time.time()-self.t0), append(self.line.get_ydata(), data))
+		self.ax.set_xlim(0, time.time()-self.t0)
+		self.ax.set_ylim(min(self.line.get_ydata()), max(self.line.get_ydata()))
+		self.plot.getFig().canvas.draw()
+		self.plot.getFig().canvas.flush_events()
+		# self.ax.draw_artist(self.ax.patch)
+		# self.ax.draw_artist(self.line)
+		# self.plot.getFig().canvas.update()
+		# self.plot.getFig().canvas.flush_events()
 		return
 
 
